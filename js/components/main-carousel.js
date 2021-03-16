784e4683e989af1663af562976270d8a7bdaf3e8
victorsouza-mnz <victorsouzamnz@gmail.com>
2021-03-15 22:32:37 -0300

    concerta bug carousel

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -3,12 +3,12 @@ const btnLeft = document.querySelector('.arrow-btn.-left')
 const elements = document.querySelector('.elements')
 let deslocamento = 0
 btnRight.addEventListener('click', e => {
-    deslocamento += 100
+    deslocamento -= 100
     elements.style = `transform: translateX(${deslocamento}px)`
 })
 
 
 btnLeft.addEventListener('click', e => {
-    deslocamento -= 100
+    deslocamento += 100
     elements.style = `transform: translateX(${deslocamento}px)`
 })
